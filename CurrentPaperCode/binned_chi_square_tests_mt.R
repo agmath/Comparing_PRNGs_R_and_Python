db3b3b4d0c2b6bdb72410f536d68d4577d9a36af
Adam Gilbert <agilbertmath@gmail.com>
2026-05-06 07:47:03 -0400

    binned chi-square update

--- CurrentPaperCode/binned_chi_square_tests_mt.R
+++ CurrentPaperCode/binned_chi_square_tests_mt.R
@@ -21,11 +21,22 @@ binned_chi_square_tests_mt <- function(num_seqs = 10,
   #Create bins
   bin_breaks <- seq(min_rand, max_rand, length.out = num_bins + 1)
   
+  
   #Initialize vector to store results
-  chi_sq_results <- c()
+  chi_sq_test_statistics <- c()
+  chi_sq_p_values <- c()
+  
+  #Degrees of Freedom for the test
+  df <- num_bins - 1
+  
+  #Critical Value at 95% significance
+  critical_val <- qchisq(0.95, df)
+  
   
   #Loop through each generated sequence
   for(i in 1:ncol(pRNGseqs_mt)){
+
+    
     
     #Calculate observed counts
     obs_counts <- table(cut(pRNGseqs_mt[[i]], breaks = bin_breaks, include.lowest = TRUE))
@@ -34,26 +45,27 @@ binned_chi_square_tests_mt <- function(num_seqs = 10,
     exp_counts <- rep(sum(obs_counts)/num_bins, num_bins)
     
     #Compute Chi-Square statistic
-    chi_sq_results[i] <- sum((obs_counts - exp_counts)^2/exp_counts)
+    chi_sq_test_statistics[i] <- sum((obs_counts - exp_counts)^2/exp_counts)
+    
+    #pvalue for the results
+    chi_sq_p_values[i] <- pchisq(chi_sq_test_statistics[i], df = df, lower.tail = FALSE)
+    
   }
   
-  #Degrees of Freedom
-  df <- num_bins - 1
+
   
-  #Critical Value at 95% significance
-  critical_val <- qchisq(0.95, df)
-  
+
   #Combine Results
-  
   results_df <- data.frame(
     Column = colnames(pRNGseqs_mt),
-    Chi_Square_Statistic = chi_sq_results,
+    Chi_Square_Statistic = chi_sq_test_statistics,
+    Chi_Square_PValue = chi_sq_p_values,
     DF = df,
     Critical_Value_95 = critical_val
   ) %>%
     mutate(
       Reject_H0 = (Chi_Square_Statistic > Critical_Value_95)
     )
-  
+
   return(results_df)
 }
